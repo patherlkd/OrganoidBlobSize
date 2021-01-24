848abe501f3c6cfb3f0db2baaa59fb368df753c9
Luke Davis <luke.davis.16@ucl.ac.uk>
2021-01-24 19:00:16 +0100

    Add files via upload

--- Example_workflow.py
+++ Example_workflow.py
@@ -12,7 +12,7 @@ from os.path import isfile, join
 from pathlib import Path
 import cv2
 
-folder = "images/"
+folder = "TasiaImages2/"
 imagefiles = [f for f in listdir(folder) if isfile(join(folder, f))]
 
 basenames = [Path(name).stem for name in imagefiles] 
@@ -31,6 +31,8 @@ for i in imagefiles:
 #images[-1].show()
 
 length_of_pixel = 1.86 # micro meters
+#length_of_pixel = 0.93 # microns
+
 area_per_pixel = length_of_pixel**2 # micro meters^2
 number_of_pixels = (np_im_arrays[-1].shape[0]*np_im_arrays[-1].shape[1])
 
@@ -44,7 +46,8 @@ print("Saving filtered images in folder: filtered_images/")
 cnt = 0
 
 for np_array in np_im_arrays:
-	channel_array = ImLD.grab_intensity_band(np_array,In_min,In_max)
+	channel_array = ImLD.grab_intensity_band_binary(np_array,In_min,In_max)
+
 	chanimage = Image.fromarray(channel_array).convert('RGB')
 	print("[] Saving image (threshold filtered): "+"filtered_images/"+'filtered_'+basenames[cnt]+'.png')
 	chanimage.save("filtered_images/"+'filtered_'+basenames[cnt]+'.png')
@@ -62,22 +65,24 @@ params.minThreshold = In_min
 params.maxThreshold = In_max
 
 
-# Filter by Area.
+# Filter by Area (numerical value is in microns)
 params.filterByArea = True
-params.minArea = 1000
-params.maxArea = 10000000
+params.minArea = int(100000/area_per_pixel)
+params.maxArea = int(2000000/area_per_pixel)
 
 # Filter by Circularity
 params.filterByCircularity = False
-params.minCircularity = 0.1
+params.minCircularity = 0.5
+params.maxCircularity = 1.0
 
 # Filter by Convexity
 params.filterByConvexity = False
-params.minConvexity = 0.87
+params.minConvexity = 0.1
+params.maxConvexity = 1.0
 
 # Filter by Inertia
-params.filterByInertia = True
-params.minInertiaRatio = 0.2
+params.filterByInertia = False
+params.minInertiaRatio = 0.475
 params.maxInertiaRatio = 1.0
 
 print("Detecting organoid blob...")
@@ -99,23 +104,29 @@ for cnt in range(len(np_im_arrays)):
 	pts = cv2.KeyPoint_convert(keypoints)
 
 	# Get the diameters and areas (usually just one of each - for one blob)
+
 	dias = []
 	areas = []
 	for kp in keypoints:
-		dia = kp.size
-		dias.append(dia)
-		areas.append(np.pi*(dia*0.5)**2) # Area of circle = pi*radius^2
+		radius = kp.size
+		dias.append(radius*2.0)
+		areas.append(np.pi*(radius)**2) # Area of circle = pi*radius^2
 
 
 	# Draw detected blobs as red circles.
 	# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
 	im_with_keypoints = cv2.drawKeypoints(timage, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-	
+	print("[] Computing area for "+basenames[cnt])
 	with open("data/"+"area_"+basenames[cnt]+'.txt','w') as f:
 		for area in areas:
 			f.write(str(area)+'\n')
-
+	
+	print("[] Computing diameter for "+basenames[cnt])
+	with open("data/"+"diameter_"+basenames[cnt]+'.txt','w') as f:
+		for dia in dias:
+			f.write(str(dia)+'\n')
+ 
 	print("[] Saving image (with a red circle around organoid blob): "+"blob_images/"+"blob_"+basenames[cnt]+'.png')
 	cv2.imwrite("blob_images/"+"blob_"+basenames[cnt]+'.png', im_with_keypoints)
 
